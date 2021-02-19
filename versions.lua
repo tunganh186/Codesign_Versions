24f7106b6914cbb04e1cd8174a7c5d41f47797f0
Ramp <ramp.clash@gmail.com>
2021-02-19 12:02:39 +0000

    Create versions.lua

---
+++ versions.lua
@@ -0,0 +1,11 @@
+{
+    ['cd_carhud'] = '3.0.2',
+    ['cd_dispatch'] = '3.0.2',
+    ['cd_garage'] = '3.0.3',
+    ['cd_identity'] = '1.0.1',
+    ['cd_multicharacter'] = '3.0.1',
+    ['cd_playerhud'] = '3.0.1',
+    ['cd_props'] = '3.0.2',
+    ['cd_spawnselect'] = '3.0.1',
+    ['cd_terminalhacker'] = '3.0.1',
+}
